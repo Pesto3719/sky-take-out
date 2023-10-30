73c81c5ebabdbc45948a1485cd9866437cab231c
p1sto <zhiyangl3719@163.com>
2023-10-30 19:43:46 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -12,19 +12,22 @@ public interface EmployeeMapper {
 
     /**
      * 根据用户名查询员工
+     *
      * @param username
      * @return
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
-    @Insert("insert into employee"+
-            "(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)"+
-            "values"+
-    "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime},#{updateTime},#{createUser}, #{updateUser})")
+
+    @Insert("insert into employee" +
+            "(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)" +
+            "values" +
+            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime},#{updateTime},#{createUser}, #{updateUser})")
     void insert(Employee employee);
 
     /**
      * 分页查询
+     *
      * @param employeePageQueryDTO
      * @return
      */
@@ -34,4 +37,13 @@ public interface EmployeeMapper {
      * 根据主键动态修改属性
      */
     void update(Employee employee);
+
+    /**
+     * 根据id查询员工信息
+     *
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
